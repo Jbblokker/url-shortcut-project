6ccd941606925916df699afd9f883dc43d389304
Jonathan Blokker <jbblokker@outlook.com>
2023-07-18 20:51:13 +0200

    Add Firebase
    Added Firebase to my project for storage of my URLS.

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'urlshortener-c35b6',
+    appId: '1:27892657508:web:f2a8bbee07afc4720c5e71',
+    storageBucket: 'urlshortener-c35b6.appspot.com',
+    apiKey: 'AIzaSyDCE-krt4JwiuJbin8fMrVqktUdB8pdv8E',
+    authDomain: 'urlshortener-c35b6.firebaseapp.com',
+    messagingSenderId: '27892657508',
+    measurementId: 'G-FZ1P6W44FN',
+  },
+};

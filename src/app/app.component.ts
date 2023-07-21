e1b43e8a81318d6744118d43f9329b6cb8d244c6
Jonathan Blokker <jbblokker@outlook.com>
2023-07-21 07:50:59 +0200

    removed firebase from app.ts
    removed the firebase functions and into a service to be used more easily

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Firestore, collection, addDoc, collectionData } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'url-shortcut-project';
-  userUrl?: Observable<any>;
-
-  constructor(private firestore: Firestore){
-    this.getData()
-  }
-
-
-  getData() {
-    const urlCollection = collection(this.firestore, 'your-shortcuts')
-    collectionData(urlCollection).subscribe(val => {
-      console.log(val)
-    })
-
-    this.userUrl = collectionData(urlCollection);
-  }
-}
+export class AppComponent {}

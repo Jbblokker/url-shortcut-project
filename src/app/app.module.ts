3f678d2dbcea046fc17f254c0c5e57f23c5d8863
Jonathan Blokker <jbblokker@outlook.com>
2023-07-21 07:51:50 +0200

    Modified main app folder
    added components to the html file and made minor scss tweaks for overall design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,25 @@ import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { HttpClientModule } from '@angular/common/http'
+import { UrlInputComponent } from "./url-input/url-input.component";
+import { UrlHistoryDisplayComponent } from "./url-history-display/url-history-display.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore())
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        HttpClientModule,
+        BrowserModule,
+        AppRoutingModule,
+        provideFirebaseApp(() => initializeApp(environment.firebase)),
+        provideAuth(() => getAuth()),
+        provideFirestore(() => getFirestore()),
+        UrlInputComponent,
+        UrlHistoryDisplayComponent
+    ]
 })
 export class AppModule { }
